4e926293f5fcc22e85f174d6c6be8ec80c0f7506
Ru Chern Chong <hello@ruchern.dev>
2026-04-15 02:33:21 +0800

    feat: add node server and path alias support

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { FOO } from "@/constants";
 
-const app = new Hono()
+const app = new Hono();
 
 const welcomeStrings = [
-  'Hello Hono!',
-  'To learn more about Hono on Vercel, visit https://vercel.com/docs/frameworks/backend/hono'
-]
+  "Hello Hono!",
+  "To learn more about Hono on Vercel, visit https://vercel.com/docs/frameworks/backend/hono",
+  FOO,
+];
 
-app.get('/', (c) => {
-  return c.text(welcomeStrings.join('\n\n'))
-})
+app.get("/", (c) => {
+  return c.text(welcomeStrings.join("\n\n"));
+});
 
-export default app
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
+
+export default app;
